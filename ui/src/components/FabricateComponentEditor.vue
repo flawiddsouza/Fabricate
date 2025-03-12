325147bbd63f11037066363653fd7ae99ecc91d9
Flawid DSouza <me@flawiddsouza.com>
2025-03-12 11:47:02 +0530

    component editor > tabs instead of sections as it's overwhelming to see everything together

--- ui/src/components/FabricateComponentEditor.vue
+++ ui/src/components/FabricateComponentEditor.vue
@@ -1,51 +1,74 @@
 <template>
   <div>
-    <div>
-      <label>Name<br>
-        <input type="text" v-model="modelValue.name" />
-      </label>
+    <ul class="editor-tabs">
+      <li :class="{ active: activeTab === 'name' }" @click="activeTab = 'name'">Name</li>
+      <li :class="{ active: activeTab === 'props' }" @click="activeTab = 'props'">Props</li>
+      <li :class="{ active: activeTab === 'variables' }" @click="activeTab = 'variables'">Variables</li>
+      <li :class="{ active: activeTab === 'computed' }" @click="activeTab = 'computed'">Computed</li>
+      <li :class="{ active: activeTab === 'constants' }" @click="activeTab = 'constants'">Constants</li>
+      <li :class="{ active: activeTab === 'nodes' }" @click="activeTab = 'nodes'">Nodes</li>
+      <li :class="{ active: activeTab === 'script' }" @click="activeTab = 'script'">Script</li>
+    </ul>
+
+    <div v-if="activeTab === 'name'">
+      <div>
+        <label>Name<br>
+          <input type="text" v-model="modelValue.name" />
+        </label>
+      </div>
     </div>
 
-    <div style="margin-top: 1rem;">
-      <label>Props (JSON)<br>
-        <textarea v-model="propsJson"></textarea>
-      </label>
+    <div v-else-if="activeTab === 'props'">
+      <div style="margin-top: 1rem;">
+        <label>Props (JSON)<br>
+          <textarea v-model="propsJson"></textarea>
+        </label>
+      </div>
     </div>
 
-    <div style="margin-top: 1rem;">
-      <label>Variables (JSON)<br>
-        <textarea v-model="variablesJson"></textarea>
-      </label>
+    <div v-else-if="activeTab === 'variables'">
+      <div style="margin-top: 1rem;">
+        <label>Variables (JSON)<br>
+          <textarea v-model="variablesJson"></textarea>
+        </label>
+      </div>
     </div>
 
-    <div style="margin-top: 1rem;">
-      <label>Computed (JSON)<br>
-        <textarea v-model="computedJson"></textarea>
-      </label>
+    <div v-else-if="activeTab === 'computed'">
+      <div style="margin-top: 1rem;">
+        <label>Computed (JSON)<br>
+          <textarea v-model="computedJson"></textarea>
+        </label>
+      </div>
     </div>
 
-    <div style="margin-top: 1rem;">
-      <label>Constants (JSON)<br>
-        <textarea v-model="constantsJson"></textarea>
-      </label>
+    <div v-else-if="activeTab === 'constants'">
+      <div style="margin-top: 1rem;">
+        <label>Constants (JSON)<br>
+          <textarea v-model="constantsJson"></textarea>
+        </label>
+      </div>
     </div>
 
-    <div style="margin-top: 1rem;">
-      <div><label>Nodes</label></div>
-      <FabricateComponentNodeEditor v-model="modelValue.nodes" />
-      <button @click="handleAddNode">Add node</button>
+    <div v-else-if="activeTab === 'nodes'">
+      <div style="margin-top: 1rem;">
+        <FabricateComponentNodeEditor v-model="modelValue.nodes" />
+        <button @click="handleAddNode">Add node</button>
+      </div>
     </div>
 
-    <div style="margin-top: 1rem;">
-      <label>Script<br>
-        <textarea v-model="modelValue.script"></textarea>
-      </label>
+    <div v-else-if="activeTab === 'script'">
+      <div style="margin-top: 1rem;">
+        <label>Script<br>
+          <textarea v-model="modelValue.script"></textarea>
+        </label>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { watch, computed } from 'vue'
+import { watch, computed, ref } from 'vue'
 import FabricateComponentNodeEditor from './FabricateComponentNodeEditor.vue'
 
 const props = defineProps<{
@@ -53,6 +76,8 @@ const props = defineProps<{
 }>()
 
 const emit = defineEmits(['update:modelValue'])
+
+const activeTab = ref('name')
 
 const variablesJson = computed({
   get: () => JSON.stringify(props.modelValue.variables, null, 2),
@@ -128,5 +153,18 @@ textarea {
   height: 15rem;
   font-family: monospace;
   resize: vertical;
+}
+
+.editor-tabs {
+  display: flex;
+  list-style: none;
+  padding: 0;
+}
+.editor-tabs li {
+  margin-right: 1rem;
+  cursor: pointer;
+}
+.editor-tabs li.active {
+  font-weight: bold;
 }
 </style>
